ffd732fde07a1f17b4c3f226bd2f31af5eefeb21
AdvenAdam <advenadam@student.uns.ac.id>
2024-10-25 06:53:23 +0000

    update unique key and salt

--- wp-config.php
+++ wp-config.php
@@ -57,14 +57,14 @@ define( 'WP_SITEURL', 'http://dev.triconville.com' );
 *
 * @since 2.6.0
 */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'bBD`h+V1b9M2o?Zc.m83>+~MS?i/9AEOWh)/k#}LCqLW2g(;k58RqYG>TaITjE.H');
+define('SECURE_AUTH_KEY',  '`Q~/:7q/9%K|,`+1*bE}w^Rrq;T jEPOV;=o}@PiE`9/emm+<Lwz@q6/LX<jp>S[');
+define('LOGGED_IN_KEY',    'Vr//5[D5~vAv8FHc*(<=R:O4WzSg87.c=BB{F4/#;D>mNr2o&yW7kby0,qXRqrJz');
+define('NONCE_KEY',        'muV1on(:IgEP3^j@-8+|rW)Hgs*75)$])Y^lU3y!sa<FuW}_~hY%Jrmc@>3 p8_q');
+define('AUTH_SALT',        './!Sab0<Q{AWY0l!i|YnLHa8trCl!&=bOf}fxkcZ_o|Zhe|(UE9h##fLJG~S+fOV');
+define('SECURE_AUTH_SALT', '{6fgj+8N~!&fo3|2vo]=:#l?sl|9XS=imaDC$|]M|fC6Q<)3.^|<5>4Zyr842,a,');
+define('LOGGED_IN_SALT',   '?vfosI|`0Yi}s0VS%_K0xiN%,I]{NLVT7 hF(>(WtC=r2nq-DflQ& +`cRR3@(?,');
+define('NONCE_SALT',       '_]|1TT9qtcp_#QEY$I1x$X$nE!} LWDqI}>{>-_--hywiW9txl(.iO,81}!@_Cx}');
 
 /**#@-*/
 
