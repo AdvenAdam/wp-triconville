f974f047c66ce1e06c3ae0ce4985f3efc7b11831
AdvenAdam <advenadam@student.uns.ac.id>
2024-10-28 11:19:46 +0700

    update news template

--- wp-content/themes/twentytwentyfour/ven-news.php
+++ wp-content/themes/twentytwentyfour/ven-news.php
@@ -10,6 +10,22 @@ get_template_part('header-custom');
 
 <div class="px-3 md:px-5">
     <div class="max-w-[1440px] mx-auto">
+        <div class="inline-flex gap-1 items-center mb-5">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 fill="none"
+                 viewBox="0 0 24 24"
+                 stroke-width="1.5"
+                 stroke="currentColor"
+                 class="size-4">
+                <path stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15.75 19.5 8.25 12l7.5-7.5" />
+            </svg>
+            <a href="javascript:history.back()"
+               class="text-sm">
+                <h5>Back</h5>
+            </a>
+        </div>
         <div class="content-wrapper">
             <?php while (have_posts()) : the_post(); ?>
             <?php the_content(); ?>
@@ -43,6 +59,10 @@ get_template_part('header-custom');
         </div>
     </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js"></script>
+<script>
+let clipboard = new ClipboardJS('.btn');
+</script>
 <?php
 // Include your custom footer
 get_template_part('footer-custom');
