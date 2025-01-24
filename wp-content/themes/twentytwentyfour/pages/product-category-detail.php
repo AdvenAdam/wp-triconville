7b7c382f6495f965d6f055a09c824d7ffc0b8a61
AdvenAdam <advenadam@student.uns.ac.id>
2025-01-24 13:06:19 +0700

    update sitemap

--- wp-content/themes/twentytwentyfour/pages/product-category-detail.php
+++ wp-content/themes/twentytwentyfour/pages/product-category-detail.php
@@ -62,6 +62,7 @@ let localProductsData = [];
 let categoriesData;
 let haveSubCategories = false;
 let productListSelected = [];
+let slugs = []
 
 $(document).ready(function() {
     categoriesData = <?= json_encode($selectedCategory[0]); ?>;
@@ -86,6 +87,8 @@ function renderMaster() {
     } catch (error) {
         console.error("🚀 ~ renderMaster ~ error:", error)
         redirectError()
+    } finally {
+        console.log("🚀 ~ slugs:", slugs)
     }
 }
 
@@ -209,6 +212,7 @@ function renderProducts(data, headerTitle = 'All Types') {
     `);
 
     data.sort((a, b) => a.name.localeCompare(b.name)).forEach((e, index) => {
+        slugs.push(slugify(e.name));
         $(`#product__list__${slugify(headerTitle)}`).append(`
             <a href= "<?= BASE_LINK; ?>/product-detail/${slugify(e.name)}"
                 class="product__${slugify(headerTitle)}"
