f136aa28d7093f3dbeb8c8897221d7030f3f7bd6
root <root@dev-wp-tcv-2.asia-southeast1-b.c.triconville-akeneo.internal>
2025-03-20 04:11:53 +0000

    new env

--- wp-config.php
+++ wp-config.php
@@ -1,36 +1,36 @@
 <?php
 
 /**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the website, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * Database settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://wordpress.org/documentation/article/editing-wp-config-php/
- *
- * @package WordPress
- */
+* The base configuration for WordPress
+*
+* The wp-config.php creation script uses this file during the installation.
+* You don't have to use the website, you can copy this file to "wp-config.php"
+* and fill in the values.
+*
+* This file contains the following configurations:
+*
+* * Database settings
+* * Secret keys
+* * Database table prefix
+* * ABSPATH
+*
+* @link https://wordpress.org/documentation/article/editing-wp-config-php/
+*
+* @package WordPress
+*/
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'wp_triconville');
 
 /** Database username */
-define('DB_USER', 'wordpress');
+define('DB_USER', 'awan');
 
 /** Database password */
-define('DB_PASSWORD', 'wordpress');
+define('DB_PASSWORD', 'ins-awan@srg');
 
 /** Database hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '34.143.245.128');
 
 /** Database charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -48,16 +48,16 @@ define('WP_SITEURL', 'https://triconville.com');
 define('ENV', 'prod');
 
 /**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+* Authentication unique keys and salts.
+*
+* Change these to different unique phrases! You can generate these using
+* the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+*
+* You can change these at any point in time to invalidate all existing cookies.
+* This will force all users to have to log in again.
+*
+* @since 2.6.0
+*/
 define('AUTH_KEY',         'bBD`h+V1b9M2o?Zc.m83>+~MS?i/9AEOWh)/k#}LCqLW2g(;k58RqYG>TaITjE.H');
 define('SECURE_AUTH_KEY',  '`Q~/:7q/9%K|,`+1*bE}w^Rrq;T jEPOV;=o}@PiE`9/emm+<Lwz@q6/LX<jp>S[');
 define('LOGGED_IN_KEY',    'Vr//5[D5~vAv8FHc*(<=R:O4WzSg87.c=BB{F4/#;D>mNr2o&yW7kby0,qXRqrJz');
@@ -70,37 +70,35 @@ define('NONCE_SALT',       '_]|1TT9qtcp_#QEY$I1x$X$nE!} LWDqI}>{>-_--hywiW9txl(.
 /**#@-*/
 
 /**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
+* WordPress database table prefix.
+*
+* You can have multiple installations in one database if you give each
+* a unique prefix. Only numbers, letters, and underscores please!
+*/
 $table_prefix = 'wp_';
 
 /**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
- */
+* For developers: WordPress debugging mode.
+*
+* Change this to true to enable the display of notices during development.
+* It is strongly recommended that plugin and theme developers use WP_DEBUG
+* in their development environments.
+*
+* For information on other constants that can be used for debugging,
+* visit the documentation.
+*
+* @link https://wordpress.org/documentation/article/debugging-in-wordpress/
+*/
 define('WP_DEBUG', false);
 define('WP_DEBUG_LOG', false);
 define('WP_DEBUG_DISPLAY', false);
 /* Add any custom values between this line and the "stop editing" line. */
 
-define('WP_CACHE', true); // Added by WP Cloudflare Super Page Cache
-
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
 if (!defined('ABSPATH')) {
-	define('ABSPATH', __DIR__ . '/');
+define('ABSPATH', __DIR__ . '/');
 }
 
 /** Sets up WordPress vars and included files. */
